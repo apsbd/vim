e7deb9da088d81c8dac06942e4f28731e52695ed
Mohiuddin Ahmed <mohiuddin_ahmed_07@yahoo.com>
2020-09-19 13:44:53 +0600

    wip

--- vimconf.sh
+++ vimconf.sh
@@ -4,5 +4,5 @@ if test ! -d "$DIR"; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-    cd ~/ && git clone ""
+    cd ~/ && git clone "https://github.com/apsbd/vim/blob/master/.vimrc"
 fi
