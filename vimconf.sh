87938eb22607f415055c99e8947ebe78781a18e4
Mohiuddin Ahmed <mohiuddin_ahmed_07@yahoo.com>
2020-09-19 14:06:55 +0600

    added vim plug install command

--- vimconf.sh
+++ vimconf.sh
@@ -4,5 +4,7 @@ if test ! -d "$DIR"; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
 
-    curl -fLo ~/.vimrc "https://raw.githubusercontent.com/apsbd/vim/master/.vimrc"
+    curl -fLo ~/.vimrc "https://raw.githubusercontent.com/apsbd/vim/master/.vimrc" &&
+
+    vim +PlugInstall +qall
 fi
